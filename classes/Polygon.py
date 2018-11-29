abc64c6da489aa3192da26db5b62f53c12546f64
Valentyn Khoroshchak <smartgtsua@gmail.com>
2018-11-30 01:34:36 +0200

    Added new windows, nice print and some changes

--- classes/Polygon.py
+++ classes/Polygon.py
@@ -1,12 +1,11 @@
 from classes.Line import Line
-from classes.Point import Point
-import math
 
 
 class Polygon(object):
-    def __init__(self, points):
+    def __init__(self, points, priority):
         self.count = len(points)
         self.edges = []
+        self.priority = priority
         for i in range(self.count - 1):
             self.edges.append(Line(points[i], points[i + 1]))
 
@@ -30,55 +29,58 @@ class Polygon(object):
                 pos_count += 1
             else:
                 neg_count += 1
+
         if pos_count and neg_count:
             return False
         else:
             return True
 
-    # Реализаця алгоритма!
+    @property
+    def get_priority(self):
+        return self.priority
+
     def cyruse_beck(self, l):
         t_begin = 0.0
         t_end = 1.0
-        dirL = l.direction
+        dir_l = l.direction
 
         for edge in self.edges:
-            if edge.normal.dot(dirL) < 0:
+            if edge.normal.dot(dir_l) < 0:
                 t = l.intersection_parameter(edge)
                 if t > t_begin:
                     t_begin = t
 
-            if edge.normal.dot(dirL) > 0:
+            if edge.normal.dot(dir_l) > 0:
                 t = l.intersection_parameter(edge)
                 if t < t_end:
                     t_end = t
 
-            if edge.normal.dot(dirL) == 0:
+            if edge.normal.dot(dir_l) == 0:
                 if not edge.on_left(l.begin):
                     return False, l
-
-        l = l.morph(t_begin, t_end)
         if t_begin > t_end:
             return False, l
 
+        l = l.morph(t_begin, t_end)
         return True, l
 
     def cyruse_beck_out(self, l):
         t_begin = 0.0
         t_end = 1.0
-        dirL = l.direction
+        dir_l = l.direction
 
         for edge in self.edges:
-            if edge.normal.dot(dirL) < 0:
+            if edge.normal.dot(dir_l) < 0:
                 t = l.intersection_parameter(edge)
                 if t > t_begin:
                     t_begin = t
 
-            if edge.normal.dot(dirL) > 0:
+            if edge.normal.dot(dir_l) > 0:
                 t = l.intersection_parameter(edge)
                 if t < t_end:
                     t_end = t
 
-            if edge.normal.dot(dirL) == 0:
+            if edge.normal.dot(dir_l) == 0:
                 if not edge.on_left(l.begin):
                     return False, l
 
--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
 import random
+import copy
 
 from classes.Polygon import Polygon
 from classes.Point import Point
 from classes.Line import Line
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
 
-def main():
-    p = Polygon([Point(0, 0), Point(2, 2), Point(1, 0), Point(3, -1)])
-    p2 = Polygon([Point(0, 0), Point(2, 0), Point(2, 2), Point(1, 4), Point(0, 2)])
-    p.output()
-    print("Is convex: ", p.is_convex())
-    print("Is convex: ", p2.is_convex())
+
+def draw_windows():
     verticals = [
         (0., 0.),
         (2., 0.),
@@ -32,44 +31,129 @@ def main():
              ]
 
     path = Path(verticals, codes)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    patch = patches.PathPatch(path, fill=None, lw=2)
+    patch = patches.PathPatch(path, fill=None, lw=2, zorder=5)
     ax.add_patch(patch)
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
+
+    verticals = [
+        (0., 0.),
+        (1., 0.),
+        (1.5, 0.5),
+        (1, 1),
+        (0., 1.),
+        (0., 0.),
+    ]
+
+    codes = [Path.MOVETO,
+             Path.LINETO,
+             Path.LINETO,
+             Path.LINETO,
+             Path.LINETO,
+             Path.CLOSEPOLY,
+             ]
+
+    path = Path(verticals, codes)
+
+    patch = patches.PathPatch(path, fill=None, lw=2, zorder=10)
+    ax.add_patch(patch)
+
+    verticals = [
+        (3., 0.),
+        (6., 0.),
+        (7., 2.),
+        (4.5, 3.),
+        (2., 2.),
+        (3., 0.),
+    ]
+
+    codes = [Path.MOVETO,
+             Path.LINETO,
+             Path.LINETO,
+             Path.LINETO,
+             Path.LINETO,
+             Path.CLOSEPOLY,
+             ]
+
+    path = Path(verticals, codes)
+
+    patch = patches.PathPatch(path, fill=None, lw=2, zorder=10)
+    ax.add_patch(patch)
+
+    ax.set_xlim(-5, 8)
+    ax.set_ylim(-5, 8)
+
+
+def out_print(i, p, count, colors):
+    result, b = p.cyruse_beck_out(i)
+    if i.begin.x != b.begin.x and i.begin.y != b.begin.y:
+        plt.plot([i.begin.x, b.begin.x], [i.begin.y, b.begin.y], 'k-', lw=1,
+                 color=(random.random(), random.random(), random.random()))
+
+    if i.end.x != b.end.x and i.end.y != b.end.y:
+        plt.plot([i.end.x, b.end.x], [i.end.y, b.end.y], 'k-', lw=1,
+                 color=(random.random(), random.random(), random.random()))
+
+    if not result:
+        plt.plot([i.begin.x, i.end.x], [i.begin.y, i.end.y], 'k-', lw=1,
+                 color=(random.random(), random.random(), random.random()))
+
+    print("Line: %s %s" % (i, result))
+
+
+def in_print(i, p, count, colors):
+    result, b = p.cyruse_beck(i)
+    if result:
+        i = b
+        plt.plot([i.begin.x, i.end.x], [i.begin.y, i.end.y], 'k-', lw=1,
+                 color=(colors[count], colors[count + 1], colors[count + 2]))
+    count += 3
+
+
+def main():
+    p = Polygon([Point(0, 0), Point(1, 0), Point(1.5, 0.5), Point(1, 1), Point(0, 1)], 1)
+    p2 = Polygon([Point(0, 0), Point(2, 0), Point(2, 2), Point(1, 4), Point(0, 2)], 2)
+    p3 = Polygon([Point(3, 0), Point(6, 0), Point(7, 2), Point(4.5, 3), Point(2, 2)], 1)
+
+    print("Is convex: ", p.is_convex())
+    print("Is convex: ", p2.is_convex())
+    print("Is convex: ", p3.is_convex())
 
     lines = list()
+
+    lines.append(Line(Point(3.5, 0), Point(3.6, 3)))
     lines.append(Line(Point(2, 0), Point(0, 4)))
     lines.append(Line(Point(-0.5, -0.5), Point(2.5, -0.5)))
     lines.append(Line(Point(-0.5, -0.5), Point(2.5, 2.5)))
     lines.append(Line(Point(0, -1), Point(3, 2)))
     lines.append(Line(Point(0.3, 0.3), Point(0.5, 1.5)))
     lines.append(Line(Point(-2, -2), Point(20, -2)))
+    lines.append(Line(Point(-2, 0.5), Point(20, 3)))
 
-    for i in lines:
-        result, b = p2.cyruse_beck_out(i)
-        if i.begin.x != b.begin.x and i.begin.y != b.begin.y:
-            plt.plot([i.begin.x, b.begin.x], [i.begin.y, b.begin.y], 'k-', lw=2,
-                     color=(random.random(), random.random(), random.random()))
+    lines1 = copy.copy(lines)
+    lines2 = copy.copy(lines)
+    lines3 = copy.copy(lines)
 
-        if i.end.x != b.end.x and i.end.y != b.end.y:
-            plt.plot([i.end.x, b.end.x], [i.end.y, b.end.y], 'k-', lw=2,
-                     color=(random.random(), random.random(), random.random()))
+    colors = list()
+    for i in range(len(lines) * 3):
+        colors.append(random.random())
+    count = 0
 
-        if not result:
-            plt.plot([i.begin.x, i.end.x], [i.begin.y, i.end.y], 'k-', lw=2,
-                     color=(random.random(), random.random(), random.random()))
+    for i in lines1:
+        in_print(i, p, count, colors)
+        count += 3
 
-        print("Line: %s %s" % (i, result))
+    count = 0
+    for i in lines2:
+        #in_print(i, p2, count, colors)
+        out_print(i, p2, count, colors)
+        count += 3
 
-        # result,b = p2.cyruse_beck(i)
-        # if (result):
-        #     i = b
-        #     plt.plot([i.begin.x, i.end.x], [i.begin.y, i.end.y], 'k-', lw=2,
-        #               color=(random.random(), random.random(), random.random()))
+    count = 0
 
+    for i in lines3:
+        in_print(i, p3, count, colors)
+        count += 3
+
+    draw_windows()
     plt.show()
 
 
