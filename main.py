747fb206141a50c9390fecb33389fbfe88d0a76d
Valentyn Khoroshchak <smartgtsua@gmail.com>
2018-12-02 16:08:12 +0200

    Used priority and fixed some bugs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.path import Path
-import random
 import copy
 
 from classes.Polygon import Polygon
@@ -10,17 +9,15 @@ from classes.Line import Line
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
+ax.set_xlim(-5, 8)
+ax.set_ylim(-5, 8)
 
 
-def draw_windows(p2):
-    verticals = [
-        (0., 0.),
-        (2., 0.),
-        (2., 2.),
-        (1., 4.),
-        (0., 2.),
-        (0., 0.),
-    ]
+def draw_window(figure, lines):
+    verticals = list()
+    for edge in figure.edges:
+        verticals.append((edge.begin.x, edge.begin.y))
+    verticals.append((figure.edges[0].begin.x, figure.edges[0].begin.y))
 
     codes = [Path.MOVETO,
              Path.LINETO,
@@ -31,55 +28,49 @@ def draw_windows(p2):
              ]
 
     path = Path(verticals, codes)
-    patch = patches.PathPatch(path, fill=None, lw=2, zorder=5, color=p2.color)
+    patch = patches.PathPatch(path, fill=None, lw=2, zorder=5, color=figure.color)
     ax.add_patch(patch)
+    in_print(lines,figure)
+    # out_print(lines,figure)
 
-    # verticals = [
-    #     (0., 0.),
-    #     (1., 0.),
-    #     (1.5, 0.5),
-    #     (1, 1),
-    #     (0., 1.),
-    #     (0., 0.),
-    # ]
-    #
-    # codes = [Path.MOVETO,
-    #          Path.LINETO,
-    #          Path.LINETO,
-    #          Path.LINETO,
-    #          Path.LINETO,
-    #          Path.CLOSEPOLY,
-    #          ]
-    #
-    # path = Path(verticals, codes)
-    #
-    # patch = patches.PathPatch(path, fill=None, lw=2, zorder=10)
-    # ax.add_patch(patch)
-    #
-    # verticals = [
-    #     (3., 0.),
-    #     (6., 0.),
-    #     (7., 2.),
-    #     (4.5, 3.),
-    #     (2., 2.),
-    #     (3., 0.),
-    # ]
-    #
-    # codes = [Path.MOVETO,
-    #          Path.LINETO,
-    #          Path.LINETO,
-    #          Path.LINETO,
-    #          Path.LINETO,
-    #          Path.CLOSEPOLY,
-    #          ]
-    #
-    # path = Path(verticals, codes)
-    #
-    # patch = patches.PathPatch(path, fill=None, lw=2, zorder=10)
-    # ax.add_patch(patch)
 
-    ax.set_xlim(-5, 8)
-    ax.set_ylim(-5, 8)
+def draw_windows(figures):
+    for figure in figures:
+        verticals = list()
+        for edge in figure.edges:
+            verticals.append((edge.begin.x, edge.begin.y))
+        verticals.append((figure.edges[0].begin.x, figure.edges[0].begin.y))
+
+        codes = [Path.MOVETO,
+                 Path.LINETO,
+                 Path.LINETO,
+                 Path.LINETO,
+                 Path.LINETO,
+                 Path.CLOSEPOLY,
+                 ]
+
+        path = Path(verticals, codes)
+        patch = patches.PathPatch(path, fill=None, lw=3, zorder=figure.priority, color=figure.color)
+        ax.add_patch(patch)
+
+
+def print_curus(figures, lines):
+    c = 0
+    figures.sort(key=lambda x: x.priority, reverse=True)
+    lines_copy = copy.copy(lines)
+    for figure in figures:
+        lines = lines_copy
+
+        in_print(lines, figure)
+        for i in lines:
+            result, b = figure.cyruse_beck_out(i)
+            if result:
+                lines_copy.remove(i)
+                lines_copy.extend(b)
+        print('\n', c)
+        c += 1
+        for i in lines:
+            print(i)
 
 
 def out_print(lines, p):
@@ -92,7 +83,8 @@ def out_print(lines, p):
 
     for i in lines_copy:
         plt.plot([i.begin.x, i.end.x], [i.begin.y, i.end.y], 'k-', lw=1,
-                 color=i.color)
+                 color=i.color, zorder=p.priority - 1)
+    return lines_copy
 
 
 def in_print(lines, p):
@@ -101,17 +93,18 @@ def in_print(lines, p):
         if result:
             line = b
             plt.plot([line.begin.x, line.end.x], [line.begin.y, line.end.y], 'k-', lw=2,
-                     color=line.color)
+                     color=line.color, zorder=p.priority - 1)
+    return lines
 
 
 def main():
-    # p = Polygon([Point(0, 0), Point(1, 0), Point(1.5, 0.5), Point(1, 1), Point(0, 1)], 1)
-    p2 = Polygon([Point(0, 0), Point(2, 0), Point(2, 2), Point(1, 4), Point(0, 2)], 2, "black")
-    # p3 = Polygon([Point(3, 0), Point(6, 0), Point(7, 2), Point(4.5, 3), Point(2, 2)], 1)
+    figures = list()
+    figures.append(Polygon([Point(0, 0), Point(1, 0), Point(1.5, 0.5), Point(1, 1), Point(0, 1)], 1, "blue"))
+    figures.append(Polygon([Point(0, 0), Point(2, 0), Point(2, 2), Point(1, 4), Point(0, 2)], 5, "black"))
+    figures.append(Polygon([Point(3, 0), Point(6, 0), Point(7, 2), Point(4.5, 3), Point(2, 2)], 0, "grey"))
 
-    # print("Is convex: ", p.is_convex())
-    print("Is convex: ", p2.is_convex())
-    # print("Is convex: ", p3.is_convex())
+    for i in figures:
+        print("Is convex: ", i.is_convex())
 
     lines = list()
 
@@ -124,10 +117,11 @@ def main():
     lines.append(Line(Point(-2, -2), Point(20, -2), "aqua"))
     lines.append(Line(Point(-2, 0.5), Point(20, 3), "pink"))
 
-    out_print(lines, p2)
-    # in_print(lines,p2)
+    draw_window(figures[1], lines)
 
-    draw_windows(p2)
+    # print_curus(figures, lines)
+    # draw_windows(figures)
+
     plt.show()
 
 
